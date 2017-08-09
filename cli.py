102c23c2d3ee1c4c7e9c97ea161fa86d1e097d38
Hector F. Jimenez <h3ct0rjs@users.noreply.github.com>
2017-08-08 19:37:43 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,7 +2,9 @@
 #@author: Prahlad Yeri
 #@description: Common functions to interface with linux cli
 #@license: MIT
-import sys,subprocess,os #,SimpleHTTPServer,SocketServer
+import sys
+import subprocess
+import os 
 
 arguments = None
 
@@ -14,12 +16,12 @@ def get_stdout(pi):
         return result[1] #some error has occured
 
 def killall(process):
-	cnt=0
-	pid =is_process_running(process)
-	while pid!=0:
+	cnt = 0
+	pid = is_process_running(process)
+	while pid! = 0:
 		execute_shell('kill ' + str(pid))
-		pid =is_process_running(process)
-		cnt+=1
+		pid = is_process_running(process)
+		cnt+= 1
 	return cnt
 
 def execute_shell(command, error=''):
@@ -28,36 +30,40 @@ def execute_shell(command, error=''):
 def execute(command='', errorstring='', wait=True, shellexec=False, ags=None):
     try:
         if (shellexec):
-            p=subprocess.Popen(command, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            p = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
             #print 'verb: ' + str(arguments.verbose)
-            if arguments.verbose: print 'command: ' + command
+            if arguments.verbose:
+		print('command: {}'.format(command))
         else:
-            p=subprocess.Popen(args=ags)
-            if arguments.verbose: print 'command: ' + ags[0]
-			
+            p = subprocess.Popen(args=ags)
+            if arguments.verbose:
+		print('command: {}'.format( ags[0]))
         if wait:
             p.wait()
-            result=get_stdout(p)
+            result = get_stdout(p)
             return result
         else:
-            if arguments.verbose: print 'not waiting'
+            if arguments.verbose:
+		print('not waiting')
             return p
+
     except subprocess.CalledProcessError as e:
-        print 'error occured:' + errorstring
+        print('An error occured: {}'.format(errorstring))
         return errorstring
+
     except Exception as ea:
-        print 'Exception occured:' + ea.message
+        print('An exception happened during execution: {}'.format(ea.message))
         return errorstring
         #show_message("Error occured: " + ea.message)
 
 def is_process_running(name):
-    cmd='ps aux |grep ' + name + ' |grep -v grep'
-    s=execute_shell(cmd)
+    cmd = 'ps aux |grep ' + name + '|grep -v grep'
+    s = execute_shell(cmd)
     #return len(s)>0
     if len(s)==0:
         return 0
     else:
-        t=s.split()
+        t = s.split()
         return int(t[1])
     
 def check_sysfile(filename):
@@ -67,7 +73,6 @@ def check_sysfile(filename):
         return '/sbin/' + filename
     else:
         return ''
-  
     
 def get_sysctl(setting):
     result = execute_shell('sysctl ' + setting)
@@ -78,14 +83,6 @@ def get_sysctl(setting):
 
 def set_sysctl(setting,value):
     return execute_shell('sysctl -w ' + setting + '=' + value)
-    
 
 def writelog(message):
 	if arguments.verbose: print message
-    #~ global tbuffer
-    #~ #tbuffer.insert_at_cursor(message + '\n')
-    #~ tbuffer.insert(tbuffer.get_end_iter(),message.strip() + '\n')
-    #~ while Gtk.events_pending():
-        #~ Gtk.main_iteration()
-    #~ #tbuffer.set_text(message + '\n')
-   
